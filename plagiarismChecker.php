75687b891951d2c074e434ab7403dbfbdc21541b
agbanusi <53221092+agbanusi@users.noreply.github.com>
2019-11-03 21:50:13 +0100

    Update plagiarismChecker.php

--- plagiarismChecker.php
+++ plagiarismChecker.php
@@ -104,8 +104,8 @@
                 <form method="post" class="form-group col-11 mt-3" name="text">
                     <textarea type="text" name="text" class="form-control rounded-0" id="FormControlTextarea" rows="15" placeholder="Type or paste here.." maxlength="1000" ></textarea>
                     <div class="col-lg-2 col-xs-12 col-sm-12 btn-3 text-center mt-5 mb-5 justify-content-center">
-                      <input type="submit" class="btn btn-primary" value="Check!">
                     </div>
+                    <input type="submit" class="btn btn-primary" value="Check!">
                 </form>
                 <div class="row container mx-3 justify-content-between mt-4">
                     <div class="col-lg-5 col-xs-12 col-sm-12 upload">
@@ -114,8 +114,8 @@
                         <input type="file" class="custom-file-input" id="customFileLang" lang="en">
                         <label class="custom-file-label" for="customFileLang"></label>
                         <div class="col-lg-2 col-xs-12 col-sm-12 btn-3 text-center mt-5 mb-5 justify-content-center">
-                          <input type="submit" class="btn btn-primary" value="Check!">
                         </div>
+                        <input type="submit" class="btn btn-primary" value="Check!">
                       </form>
                     </div>
                     <div class="col-lg-5 col-xs-12 col-sm-12 url">
@@ -128,8 +128,8 @@
                           <input type="url" class="form-control" placeholder="url"> 
                         </div>
                          <div class="col-lg-2 col-xs-12 col-sm-12 btn-3 text-center mt-5 mb-5 justify-content-center">
-                            <input type="submit" class="btn btn-primary" value="Check!">
                         </div>
+                        <input type="submit" class="btn btn-primary" value="Check!">
                       </form>
                     </div>
                 </div>
